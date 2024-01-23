1a16e5acd0d6a36efe505436285561be55415fbb
marcinstenka <mastenka@wp.pl>
2024-01-23 21:04:18 +0100

    nav animation initial

--- stenkowa/app/lib/components/nav/Nav.tsx
+++ stenkowa/app/lib/components/nav/Nav.tsx
@@ -1,23 +1,44 @@
+'use client';
+import { MutableRefObject, useEffect, useRef } from 'react';
 import styles from './nav.module.scss';
 import { MdOutlineStorage } from 'react-icons/md';
 import { RiTodoLine } from 'react-icons/ri';
 import { TbTableShortcut } from 'react-icons/tb';
 
 export default function Nav() {
+	const navItems: MutableRefObject<HTMLDivElement | null>[] = [
+		useRef(null),
+		useRef(null),
+		useRef(null),
+	];
+	const active = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		for (let i = 0; i < navItems.length; i++) {
+			const currentItem = navItems[i];
+			currentItem.current?.addEventListener('click', () => {
+				if (active.current && currentItem.current) {
+					active.current.style.left = `${currentItem.current.offsetLeft}px`;
+				}
+			});
+		}
+	}, []);
+
 	return (
 		<nav className={styles.nav}>
-			<div className={styles.nav_item}>
+			<div ref={navItems[0]} className={styles.nav_item}>
 				<MdOutlineStorage />
 				<p>Storage</p>
 			</div>
-			<div className={`${styles.nav_item} ${styles.active}`}>
+			<div ref={navItems[1]} className={`${styles.nav_item} `}>
 				<RiTodoLine />
 				<p>Todo</p>
 			</div>
-			<div className={styles.nav_item}>
+			<div ref={navItems[2]} className={styles.nav_item}>
 				<TbTableShortcut />
 				<p>Shortcuts</p>
 			</div>
+			<div ref={active} className={`${styles.active} ${styles.start}  `}></div>
 		</nav>
 	);
 }
