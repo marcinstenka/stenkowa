2752e74d4a20ab6b18ddb89f22e84b0c258f8a5a
marcinstenka <mastenka@wp.pl>
2024-01-23 12:08:57 +0100

    background improvement

--- stenkowa/app/lib/components/background/Background.tsx
+++ stenkowa/app/lib/components/background/Background.tsx
@@ -1,69 +1,77 @@
 import styles from './background.module.scss';
 export default function Background() {
+	const primary = 'rgba(0, 80, 184, 1)';
+
 	return (
-		<svg
-			className={styles.svg}
-			xmlns='http://www.w3.org/2000/svg'
-			version='1.1'
-			xmlnsXlink='http://www.w3.org/1999/xlink'
-			width='430'
-			height='900'
-			preserveAspectRatio='none'
-			viewBox='0 0 430 900'
-		>
-			<g mask='url("#SvgjsMask1002")' fill='none'>
-				<rect
-					width='430'
-					height='900'
-					x='0'
-					y='0'
-					fill='url("#SvgjsRadialGradient1003")'
-				></rect>
-				<path d='M0 0L132.88 0L0 42.42z' fill='rgba(255, 255, 255, .1)'></path>
-				<path
-					d='M0 42.42L132.88 0L133.57999999999998 0L0 280.39z'
-					fill='rgba(255, 255, 255, .075)'
-				></path>
-				<path
-					d='M0 280.39L133.57999999999998 0L310.07 0L0 297.64z'
-					fill='rgba(255, 255, 255, .05)'
-				></path>
-				<path
-					d='M0 297.64L310.07 0L333.15999999999997 0L0 506.16999999999996z'
-					fill='rgba(255, 255, 255, .025)'
-				></path>
-				<path
-					d='M430 900L404.2 900L430 583.99z'
-					fill='rgba(0, 0, 0, .1)'
-				></path>
-				<path
-					d='M430 583.99L404.2 900L375.71 900L430 464.28000000000003z'
-					fill='rgba(0, 0, 0, .075)'
-				></path>
-				<path
-					d='M430 464.28000000000003L375.71 900L365.77 900L430 421.25z'
-					fill='rgba(0, 0, 0, .05)'
-				></path>
-				<path
-					d='M430 421.25L365.77 900L197.92999999999998 900L430 249.08z'
-					fill='rgba(0, 0, 0, .025)'
-				></path>
-			</g>
-			<defs>
-				<mask id='SvgjsMask1002'>
-					<rect width='430' height='900' fill='#ffffff'></rect>
-				</mask>
-				<radialGradient
-					cx='50%'
-					cy='50%'
-					r='498.72'
-					gradientUnits='userSpaceOnUse'
-					id='SvgjsRadialGradient1003'
-				>
-					<stop stop-color='rgba(14, 42, 71, 1)' offset='0'></stop>
-					<stop stop-color='rgba(0, 80, 184, 1)' offset='0'></stop>
-				</radialGradient>
-			</defs>
-		</svg>
+		<div className={styles.container}>
+			<svg
+				className={styles.svg}
+				xmlns='http://www.w3.org/2000/svg'
+				version='1.1'
+				xmlnsXlink='http://www.w3.org/1999/xlink'
+				width='430'
+				height='900'
+				preserveAspectRatio='none'
+				viewBox='0 0 430 900'
+			>
+				<g mask='url("#SvgjsMask1110")' fill='none'>
+					<rect
+						width='430'
+						height='900'
+						x='0'
+						y='0'
+						fill='url("#SvgjsLinearGradient1111")'
+					></rect>
+					<path
+						d='M0 0L65.17 0L0 377.39z'
+						fill='rgba(255, 255, 255, .1)'
+					></path>
+					<path
+						d='M0 377.39L65.17 0L152.37 0L0 444.33z'
+						fill='rgba(255, 255, 255, .08)'
+					></path>
+					<path
+						d='M0 444.33L152.37 0L316.99 0L0 501.51z'
+						fill='rgba(255, 255, 255, .055)'
+					></path>
+					<path
+						d='M0 501.51L316.99 0L383.63 0L0 502.05z'
+						fill='rgba(255, 255, 255, .025)'
+					></path>
+					<path
+						d='M430 900L263.52 900L430 553.77z'
+						fill='rgba(0, 0, 0, 1)'
+					></path>
+					<path
+						d='M430 553.77L263.52 900L223.99999999999997 900L430 307.17999999999995z'
+						fill='rgba(0, 0, 0, .075)'
+					></path>
+					<path
+						d='M430 307.17999999999995L224 900L222.1 900L430 279.3299999999999z'
+						fill='rgba(0, 0, 0, .05)'
+					></path>
+					<path
+						d='M430 279.3299999999999L222.1 900L141.39999999999998 900L430 165.2199999999999z'
+						fill='rgba(0, 0, 0, .025)'
+					></path>
+				</g>
+				<defs>
+					<mask id='SvgjsMask1110'>
+						<rect width='430' height='900' fill='#ffffff'></rect>
+					</mask>
+					<linearGradient
+						x1='100%'
+						y1='50%'
+						x2='0%'
+						y2='50%'
+						gradientUnits='userSpaceOnUse'
+						id='SvgjsLinearGradient1111'
+					>
+						<stop stop-color='#0e2a47' offset='0'></stop>
+						<stop stop-color={primary} offset='0'></stop>
+					</linearGradient>
+				</defs>
+			</svg>
+		</div>
 	);
 }
