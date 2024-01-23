b18eaaaab497864dfd71af900220e9685395ef39
marcinstenka <mastenka@wp.pl>
2024-01-23 18:16:40 +0100

    nav styles

--- stenkowa/app/lib/components/nav/Nav.tsx
+++ stenkowa/app/lib/components/nav/Nav.tsx
@@ -1,3 +1,23 @@
+import styles from './nav.module.scss';
+import { MdOutlineStorage } from 'react-icons/md';
+import { RiTodoLine } from 'react-icons/ri';
+import { TbTableShortcut } from 'react-icons/tb';
+
 export default function Nav() {
-	return 'hello';
+	return (
+		<nav className={styles.nav}>
+			<div className={styles.nav_item}>
+				<MdOutlineStorage />
+				<p>Storage</p>
+			</div>
+			<div className={`${styles.nav_item} ${styles.active}`}>
+				<RiTodoLine />
+				<p>Todo</p>
+			</div>
+			<div className={styles.nav_item}>
+				<TbTableShortcut />
+				<p>Shortcuts</p>
+			</div>
+		</nav>
+	);
 }
