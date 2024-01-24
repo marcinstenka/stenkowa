39b8b39caa2e1334cf0e0e1dfe984f942dc3cbaf
marcinstenka <mastenka@wp.pl>
2024-01-24 11:15:14 +0100

    useNavRefs hook added

--- stenkowa/app/lib/components/nav/Nav.tsx
+++ stenkowa/app/lib/components/nav/Nav.tsx
@@ -1,36 +1,20 @@
 'use client';
-import { MutableRefObject, useEffect, useRef } from 'react';
 import styles from './nav.module.scss';
 import { MdOutlineStorage } from 'react-icons/md';
 import { RiTodoLine } from 'react-icons/ri';
 import { TbTableShortcut } from 'react-icons/tb';
+import useNavRefs from '../../hooks/useNavRefs';
 
 export default function Nav() {
-	const navItems: MutableRefObject<HTMLDivElement | null>[] = [
-		useRef(null),
-		useRef(null),
-		useRef(null),
-	];
-	const active = useRef<HTMLDivElement | null>(null);
-
-	useEffect(() => {
-		for (let i = 0; i < navItems.length; i++) {
-			const currentItem = navItems[i];
-			currentItem.current?.addEventListener('click', () => {
-				if (active.current && currentItem.current) {
-					active.current.style.left = `${currentItem.current.offsetLeft}px`;
-				}
-			});
-		}
-	}, []);
+	const { navItems, indicator } = useNavRefs({styles});
 
 	return (
 		<nav className={styles.nav}>
-			<div ref={navItems[0]} className={styles.nav_item}>
+			<div ref={navItems[0]} className={`${styles.nav_item} ${styles.active}`}>
 				<MdOutlineStorage />
 				<p>Storage</p>
 			</div>
-			<div ref={navItems[1]} className={`${styles.nav_item} `}>
+			<div ref={navItems[1]} className={styles.nav_item}>
 				<RiTodoLine />
 				<p>Todo</p>
 			</div>
@@ -38,7 +22,7 @@ export default function Nav() {
 				<TbTableShortcut />
 				<p>Shortcuts</p>
 			</div>
-			<div ref={active} className={`${styles.active} ${styles.start}  `}></div>
+			<div ref={indicator} className={styles.indicator}></div>
 		</nav>
 	);
 }
---
+++ stenkowa/app/lib/hooks/useNavRefs.ts
@@ -0,0 +1,83 @@
+import { MutableRefObject, useEffect, useRef } from 'react';
+
+type useNavRefProps = {
+	styles: { readonly [key: string]: string };
+};
+type useNavRefReturn = {
+	navItems: MutableRefObject<HTMLDivElement | null>[];
+	indicator: MutableRefObject<HTMLDivElement | null>;
+};
+export default function useNavRefs({
+	styles,
+}: useNavRefProps): useNavRefReturn {
+	const navItems: MutableRefObject<HTMLDivElement | null>[] = [
+		useRef(null),
+		useRef(null),
+		useRef(null),
+	];
+	const indicator = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		function handleResize() {
+			for (let i = 0; i < navItems.length; i++) {
+				const currentItem = navItems[i];
+				if (
+					indicator.current &&
+					currentItem.current &&
+					currentItem.current.classList.contains(styles.active)
+				) {
+					indicator.current.style.transform = `translateX(${currentItem.current.offsetLeft}px)`;
+				}
+			}
+		}
+		function handleNavItemClick(
+			currentItem: MutableRefObject<HTMLDivElement | null>,
+			i: number
+		) {
+			if (indicator.current && currentItem.current) {
+				indicator.current.style.transform = `translateX(${currentItem.current.offsetLeft}px)`;
+				indicator.current.style.width = `${currentItem.current.clientWidth}px`;
+				if (!currentItem.current.classList.contains(styles.active)) {
+					for (let j = 0; j < navItems.length; j++) {
+						if (j != i) {
+							const item = navItems[j];
+							item.current && item.current.classList.remove(styles.active);
+						}
+					}
+					currentItem.current.classList.add(styles.active);
+				}
+			}
+		}
+
+		window.addEventListener('resize', handleResize);
+
+		for (let i = 0; i < navItems.length; i++) {
+			const currentItem = navItems[i];
+			if (indicator.current && currentItem.current) {
+				if (currentItem.current.classList.contains(styles.active)) {
+					indicator.current.style.transform = `translateX(${currentItem.current.offsetLeft}px)`;
+					indicator.current.style.width = `${currentItem.current.clientWidth}px`;
+				}
+			}
+		}
+
+		for (let i = 0; i < navItems.length; i++) {
+			const currentItem = navItems[i];
+			currentItem.current?.addEventListener('click', () =>
+				handleNavItemClick(currentItem, i)
+			);
+		}
+
+		() => {
+			window.removeEventListener('resize', handleResize);
+			for (let i = 0; i < navItems.length; i++) {
+				const currentItem = navItems[i];
+				currentItem.current?.removeEventListener('click', () =>
+					handleNavItemClick(currentItem, i)
+				);
+			}
+		};
+	}, []);
+
+	return { navItems, indicator };
+}
