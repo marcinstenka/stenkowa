1fc0bfab7daf36097f6d8aa1c2f429666b16b7e3
marcinstenka <mastenka@wp.pl>
2024-01-25 09:28:53 +0100

    testing nav

--- stenkowa/app/lib/components/nav/Nav.tsx
+++ stenkowa/app/lib/components/nav/Nav.tsx
@@ -7,25 +7,36 @@ import { IoIosAdd } from 'react-icons/io';
 
 import useNavRefs from '../../hooks/useNavRefs';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
 
 export default function Nav() {
+	const pathname = usePathname();
+	const paths = ['/storage', '/todo', '/bookmarks'];
 	const { navItems, indicator } = useNavRefs({ styles });
-
+	const test1 = clsx({
+		[styles.nav_item]: true,
+		[styles.active]: pathname === paths[0],
+	});
+	const test2 = clsx({
+		[styles.nav_item]: true,
+		[styles.active]: pathname === paths[1],
+	});
+	const test3 = clsx({
+		[styles.nav_item]: true,
+		[styles.active]: pathname === paths[2],
+	});
 	return (
 		<nav className={styles.nav}>
-			<Link
-				href='/storage'
-				ref={navItems[0]}
-				className={`${styles.nav_item} ${styles.active}`}
-			>
+			<Link href={paths[0]} ref={navItems[0]} className={test1}>
 				<MdOutlineStorage />
 				<p>Storage</p>
 			</Link>
-			<Link href='/todo' ref={navItems[1]} className={styles.nav_item}>
+			<Link href={paths[1]} ref={navItems[1]} className={test2}>
 				<RiTodoLine />
 				<p>Todo</p>
 			</Link>
-			<Link href='/bookmarks' ref={navItems[2]} className={styles.nav_item}>
+			<Link href={paths[2]} ref={navItems[2]} className={test3}>
 				<TbTableShortcut />
 				<p>Bookmarks</p>
 			</Link>
