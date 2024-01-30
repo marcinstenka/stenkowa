dc5055c0fbb258d57f714f7963ed9781dda108a6
marcinstenka <mastenka@wp.pl>
2024-01-30 12:33:00 +0100

    key in StorageSection

--- stenkowa/app/lib/components/storage/StorageSection.tsx
+++ stenkowa/app/lib/components/storage/StorageSection.tsx
@@ -8,8 +8,10 @@ export default function StorageSection({ date, items }: StorageSectionProps) {
 		<section className={styles.storage_section}>
 			<h3 className={styles.storage_date}>{date}</h3>
 			<div className={styles.storage_items}>
-				{items.map((item) => (
-					<div className={styles.storage_item}>{item}</div>
+				{items.map((item, index) => (
+					<div className={styles.storage_item} key={index}>
+						{item}
+					</div>
 				))}
 			</div>
 		</section>
