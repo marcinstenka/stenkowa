8dcecb50c73acb36d913c5d27dca6ccb1c58f688
marcinstenka <mastenka@wp.pl>
2024-01-30 09:55:39 +0100

    todo refactor

--- stenkowa/app/lib/components/todo/TodoContainer.tsx
+++ stenkowa/app/lib/components/todo/TodoContainer.tsx
@@ -1,21 +1,21 @@
+import Todo from './Todo';
 import styles from './todo.module.scss';
 export default function TodoContainer() {
+	const todos = [
+		{
+			color: 'var(--primary)',
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.',
+		},
+		{
+			color: 'var(--primary)',
+			text: 'Quis autem vel eum iure reprehenderit qui in ea voluptate.',
+		},
+	];
 	return (
 		<div className={styles.container}>
-			<div className={styles.todo}>
-				<div className={styles.todo_color}></div>
-				<p className={styles.todo_text}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim.  
-				</p>
-			</div>
-			<div className={styles.todo}>
-				<div className={styles.todo_color}></div>
-				<p className={styles.todo_text}>
-					Quis autem vel eum iure reprehenderit qui in ea voluptate.
-				</p>
-			</div>
+			{todos.map((todo) => {
+				return <Todo color={todo.color} text={todo.text} />;
+			})}
 		</div>
 	);
 }
