a1441e4a67f8cbacc9081f25874d2c11fdb9a80f
marcinstenka <mastenka@wp.pl>
2024-02-01 18:19:43 +0100

    bookmarks initial view

--- stenkowa/app/lib/components/bookmarks/BookmarksContainer.tsx
+++ stenkowa/app/lib/components/bookmarks/BookmarksContainer.tsx
@@ -1,5 +1,39 @@
+import { BookmarkType } from '../../types/types';
+import Bookmark from './Bookmark';
 import styles from './bookmarks.module.scss';
-
+import { SiNetflix } from 'react-icons/si';
 export default function BookmarksContainer() {
-	return <p>Bookmarks</p>;
+	const bookmarks: BookmarkType[] = [
+		{
+			link: 'https://www.netflix.com/browse',
+			icon: 'SiNetflix',
+			text: 'Netflix',
+			color: 'var(--primary)',
+		},
+		{
+			link: 'https://www.facebook.com',
+			icon: 'FaFacebookF',
+			text: 'Facebook',
+			color: 'var(--primary)',
+		},
+		{
+			link: 'https://www.netflix.com/browse',
+			icon: 'SiNetflix',
+			text: 'Netflix',
+			color: 'var(--primary)',
+		},
+		{
+			link: 'https://www.netflix.com/browse',
+			icon: 'SiNetflix',
+			text: 'Netflix',
+			color: 'var(--primary)',
+		},
+	];
+	return (
+		<div className={styles.container}>
+			{bookmarks.map((bookmark, index) => {
+				return <Bookmark bookmark={bookmark} />;
+			})}
+		</div>
+	);
 }
--- stenkowa/app/lib/types/types.ts
+++ stenkowa/app/lib/types/types.ts
@@ -2,3 +2,9 @@ export type StorageSectionType = {
 	date: string;
 	items: string[];
 };
+export type BookmarkType = {
+	icon: string;
+	link: string;
+	color: string;
+	text: string;
+};
