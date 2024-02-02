ebc156d99ea52a310029600907fa057c28c1a9f9
marcinstenka <mastenka@wp.pl>
2024-02-02 13:33:21 +0100

    login inital view

--- stenkowa/app/lib/components/login/LoginContainer.tsx
+++ stenkowa/app/lib/components/login/LoginContainer.tsx
@@ -1,7 +1,23 @@
+import styles from './login.module.scss';
 export default function LoginContainer() {
 	return (
-		<>
-			<p>Hello from login</p>
-		</>
+		<div className={styles.container}>
+			<div className={styles.about}>
+				<div className={styles.heading}>
+					<h1>Witaj na</h1>
+					<h1>Stenkowej Stronie</h1>
+				</div>
+				<p className={styles.text}>
+					Twoja nowa, w pełni konfigurowalna strona startowa w przeglądarce.
+					<br />
+					<br />
+					Personalizuj:
+					<li>swoje skróty do innych stron</li>
+					<li>listę zadań do zrobienia</li>
+					<br />
+					Twoja nowa, w pełni konfigurowalna strona startowa w przeglądarce.
+				</p>
+			</div>
+		</div>
 	);
 }
