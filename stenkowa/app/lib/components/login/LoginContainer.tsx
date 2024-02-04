e2e725e7dc4d665a34e81d292e69aafe86fc0467
marcinstenka <mastenka@wp.pl>
2024-02-04 12:54:47 +0100

    login subpage content

--- stenkowa/app/lib/components/login/LoginContainer.tsx
+++ stenkowa/app/lib/components/login/LoginContainer.tsx
@@ -1,4 +1,29 @@
 import styles from './login.module.scss';
+
+import { CiLock } from 'react-icons/ci';
+import { MdOutlineAccountCircle } from 'react-icons/md';
+
 export default function LoginContainer() {
-	return <div className={styles.container}>login</div>;
+	return (
+		<div className={styles.container}>
+			<form className={styles.form}>
+				<div className={styles.input}>
+					<label htmlFor='userName'>Nazwa użytkownika</label>
+					<MdOutlineAccountCircle />
+					<input type='text' name='userName' />
+				</div>
+
+				<div className={styles.input}>
+					<label htmlFor='password'>Hasło</label>
+					<CiLock />
+					<input type='password' name='password' />
+				</div>
+				<div className={styles.buttons}>
+					<button className={styles.button}>Zaloguj</button>
+					<div className={styles.line}></div>
+					<button className={styles.button}>Zarejestruj</button>
+				</div>
+			</form>
+		</div>
+	);
 }
