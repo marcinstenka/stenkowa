a4a694862d3b9c1c56447306c8fab16f14c88094
marcinstenka <mastenka@wp.pl>
2024-02-06 11:39:47 +0100

    components line fix

--- stenkowa/app/todo/page.tsx
+++ stenkowa/app/todo/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import Main from '../lib/components/main/Main.1';
+import Main from '../lib/components/main/Main';
 import Background from '../lib/components/background/Background';
 
 const page = 'Todo';
