b13551d77b6677948f9ef55a75b9e20e80a9aa3f
marcinstenka <mastenka@wp.pl>
2024-02-07 09:37:41 +0100

    profile initial

--- stenkowa/app/lib/components/background/Background.tsx
+++ stenkowa/app/lib/components/background/Background.tsx
@@ -6,6 +6,33 @@ export default function Background() {
 	return (
 		<div className={styles.container}>
 			<Svg primary={primary} />
+			<div id='test' className={styles.profile}>
+				<h2>Dostosuj swój profil</h2>
+				<h3>Wybierz kolory:</h3>
+				<form className={`${styles.form} ${styles.colors}`}>
+					<input
+						className={styles.color}
+						type='color'
+						name='primary'
+						value='#0050b8'
+					/>
+					<input
+						className={styles.color}
+						type='color'
+						name='secondary'
+						value='#ffffff'
+					/>
+					<button>Zaaktualizuj</button>
+				</form>
+				<h3>Zmień dane:</h3>
+				<form className={`${styles.form} ${styles.credentials}`}>
+					<input type='text' name='userName' />
+					<input type='text' name='email' />
+					<input type='text' name='password' />
+					<button>Zaaktualizuj</button>
+				</form>
+				<div className={styles.message}></div>
+			</div>
 		</div>
 	);
 }
--- stenkowa/app/lib/components/global/Options.tsx
+++ stenkowa/app/lib/components/global/Options.tsx
@@ -1,9 +1,23 @@
+'use client';
+import { useEffect, useState } from 'react';
 import styles from './global.module.scss';
 import { CiSettings } from 'react-icons/ci';
 
 export default function Options() {
+	const [isExpanded, setIsExpanded] = useState(false);
+	useEffect(() => {
+		const p = document.getElementById('test');
+		if (isExpanded && p) {
+			p.style.margin = '0 0 0 0';
+		} else if (p) {
+			p.style.margin = '-266px 0 0 0';
+		}
+	}, [isExpanded]);
+	const togglePadding = () => {
+		setIsExpanded((prev) => !prev);
+	};
 	return (
-		<div className={styles.options}>
+		<div className={styles.options} onClick={togglePadding}>
 			<CiSettings />
 		</div>
 	);
