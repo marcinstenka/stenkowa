e0ba0ba97d07ebfc2fe0d1e55c495c4b652adfba
marcinstenka <mastenka@wp.pl>
2024-02-13 12:27:47 +0100

    Storage, todo, bookmarks layout files, home reorganized

--- stenkowa/app/lib/components/main/Main.tsx
+++ stenkowa/app/lib/components/main/Main.tsx
@@ -1,25 +1,18 @@
-import BookmarksContainer from '../bookmarks/BookmarksContainer';
 import Options from '../global/Options';
 import StenkowaTitle from '../global/StenkowaTitle';
-import StorageContainer from '../storage/StorageContainer';
-import TodoContainer from '../todo/TodoContainer';
-import HomeContainer from '../home/HomeContainer';
 import styles from './main.module.scss';
 
 type MainProps = {
-	page: string;
+	children: React.ReactNode;
 };
 
-export default function Main({ page }: MainProps) {
+export default function Main({ children }: MainProps) {
 	return (
 		<main className={styles.main}>
 			<Options />
 			<div className={styles.container} id='container'>
 				<StenkowaTitle />
-				{page == 'todo' && <TodoContainer />}
-				{page == 'storage' && <StorageContainer />}
-				{page == 'bookmarks' && <BookmarksContainer />}
-				{page == 'home' && <HomeContainer />}
+				{children}
 			</div>
 		</main>
 	);
