9a3291a47397226e1456feda31f735049288394d
marcinstenka <mastenka@wp.pl>
2024-02-23 23:03:27 +0100

    useStorageItemEdit hook & color

--- stenkowa/app/lib/components/storage/StorageItemEditForm.tsx
+++ stenkowa/app/lib/components/storage/StorageItemEditForm.tsx
@@ -7,9 +7,20 @@ import { StorageItem, TodoType } from '../../types/types';
 import useTodoEdit from '../../hooks/useTodoEdit';
 import { BiSolidEdit } from 'react-icons/bi';
 import { formatDate } from '../../functions/functions';
+import useStorageItemEdit from '../../hooks/useStorageItemEdit';
 
 export default function StorageItemEditForm(item: StorageItem) {
-	const date_added = formatDate(item.date_added, false);
+	// const date_added = formatDate(item.date_added, false);
+	const {
+		handleHeaderChange,
+		handleDetailsChange,
+		handleDateAddedChange,
+		handleColorChange,
+		details_header,
+		details,
+		date_added,
+		color,
+	} = useStorageItemEdit(item);
 	return (
 		<div className={styles.details}>
 			<div className={styles.details_header}>
@@ -18,14 +29,14 @@ export default function StorageItemEditForm(item: StorageItem) {
 						type='text'
 						name='details_header'
 						id='details_header'
-						// onChange={handleHeaderChange}
-						value={item.name}
+						onChange={handleHeaderChange}
+						value={details_header}
 						autoComplete='off'
 						minLength={3}
 						color-changing='border-color'
-						style={{ borderColor: `${item.color}` }}
+						style={{ borderColor: `${color}` }}
 					/>
-					<h4 className={styles.invisible}>{item.name}</h4>
+					<h4 className={styles.invisible}>{details_header}</h4>
 				</div>
 				<div
 					className={`${styles.date_input_container} ${styles.date_short_input_container}`}
@@ -34,34 +45,43 @@ export default function StorageItemEditForm(item: StorageItem) {
 						type='date'
 						name='details_date'
 						id='details_date'
-						defaultValue={item.date_added.toISOString().substr(0, 10)}
-						style={{ borderColor: `${item.color}` }}
+						defaultValue={date_added.toISOString().substr(0, 10)}
+						style={{ borderColor: `${color}` }}
 						color-changing='border-color'
-						// onChange={handleDateDeadlineChange}
+						onChange={handleDateAddedChange}
 					/>
 				</div>
 			</div>
 			<div className={styles.textarea_container}>
-				<div className={styles.details_text}>{item.details}</div>
+				<div className={styles.details_text}>{details}</div>
 				<textarea
 					name='details_text'
 					id='details_text'
-					// onChange={handleDetailsChange}
+					onChange={handleDetailsChange}
 				>
-					{item.details}
+					{details}
 				</textarea>
 			</div>
 
 			<div className={styles.details_lower}>
 				<p
 					className={styles.details_edit_info}
-					style={{ borderColor: `${item.color}` }}
+					style={{ borderColor: `${color}` }}
 					color-changing='border-color'
 				>
 					Kliknij element, aby zmienić
 				</p>
 				<div className={styles.details_lower_icons}>
-					<MdDelete style={{ color: `${item.color}` }} />
+					<div className={styles.input_color_container}>
+						<input
+							type='color'
+							name='details_color'
+							id='details_color'
+							value={color}
+							onChange={handleColorChange}
+						/>
+					</div>
+					<MdDelete style={{ color: `${color}` }} />
 				</div>
 			</div>
 
@@ -69,7 +89,7 @@ export default function StorageItemEditForm(item: StorageItem) {
 				<Link
 					className={styles.details_back}
 					href='/storage'
-					style={{ backgroundColor: `${item.color}` }}
+					style={{ backgroundColor: `${color}` }}
 					color-changing='background'
 				>
 					<IoReturnDownBackOutline />
@@ -77,7 +97,7 @@ export default function StorageItemEditForm(item: StorageItem) {
 				<Link
 					className={styles.details_back}
 					href='/storage'
-					style={{ backgroundColor: `${item.color}` }}
+					style={{ backgroundColor: `${color}` }}
 					color-changing='background'
 				>
 					<MdDone />
