b148d5181f9b5c08131413f0003b31ef22272fdc
marcinstenka <mastenka@wp.pl>
2024-03-13 20:54:26 +0100

    todos table

--- stenkowa/scripts/create-tables.js
+++ stenkowa/scripts/create-tables.js
@@ -1,7 +1,6 @@
 const { db } = require('@vercel/postgres');
 const users = [
 	{
-		id: '0',
 		user_name: 'Marcin',
 		email: 'marcinstenka01@gmail.com',
 		password: '123456',
@@ -9,9 +8,19 @@ const users = [
 		secondary_color: 'blue',
 	},
 ];
+const todos = [
+	{
+		name: 'Marcin',
+		user_id: '1',
+		name: '123456',
+		description: 'red',
+		color: 'blue',
+		date_deadline: new Date(),
+	},
+];
 const bcrypt = require('bcrypt');
 
-async function seedUsers(client) {
+async function createUsers(client) {
 	try {
 		await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 		// Create the "users" table if it doesn't exist
@@ -48,10 +57,47 @@ async function seedUsers(client) {
 	}
 }
 
+async function createTodos(client) {
+	try {
+		await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+		const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS todos (
+        id SERIAL PRIMARY KEY,
+		user_id INT,
+		FOREIGN KEY (user_id) REFERENCES users(id),
+        name TEXT NOT NULL,
+        description TEXT NOT NULL,
+        color TEXT NOT NULL,
+        date_deadline DATE NOT NULL
+      );
+    `;
+		console.log(`Created "todos" table`);
+		const insertedTodos = await Promise.all(
+			todos.map(async (todo) => {
+				return client.sql`
+        INSERT INTO todos (user_id, name, description, color, date_deadline)
+        VALUES (${todo.user_id}, ${todo.name}, ${todo.description}, ${todo.color},${todo.date_deadline});
+      `;
+			})
+		);
+
+		console.log(`Seeded ${insertedTodos.length} todos`);
+
+		return {
+			createTable,
+			todos: insertedTodos,
+		};
+	} catch (error) {
+		console.error('Error seeding todos:', error);
+		throw error;
+	}
+}
+
 async function main() {
 	const client = await db.connect();
 
-	await seedUsers(client);
+	await createUsers(client);
+	await createTodos(client);
 
 	await client.end();
 }
