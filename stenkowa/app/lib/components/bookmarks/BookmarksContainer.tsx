4fae92c3a81099fbaf32ff4dd8f1875645c663b8
marcinstenka <mastenka@wp.pl>
2024-03-18 20:53:33 +0100

    bookmark hover changed

--- stenkowa/app/lib/components/bookmarks/BookmarksContainer.tsx
+++ stenkowa/app/lib/components/bookmarks/BookmarksContainer.tsx
@@ -4,105 +4,11 @@ import Bookmark from './Bookmark';
 import styles from './bookmarks.module.scss';
 
 export default async function BookmarksContainer() {
-	const bookmarks: BookmarkType[] = [
-		{
-			id: 1,
-			link: 'https://www.netflix.com/browse',
-			icon: 'SiNetflix',
-			name: 'Netflix',
-			color: '#E50914',
-		},
-		{
-			id: 2,
-			link: 'https://mail.google.com/mail/u/0/',
-			icon: 'SiGmail',
-			name: 'Gmail',
-			color: '#f2a60c',
-		},
-		{
-			id: 3,
-			link: 'https://www.facebook.com',
-			icon: 'FaFacebookF',
-			name: 'Facebook',
-			color: '#4267B2',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-		{
-			id: 4,
-			link: 'https://www.youtube.com/',
-			icon: 'FaYoutube',
-			name: 'Youtube',
-			color: '#FF0000',
-		},
-	];
-	const a = await fetchBookmarks();
+	const bookmarks: BookmarkType[] | undefined = await fetchBookmarks();
 	return (
 		<div className={styles.container}>
-			{}
-			{a &&
-				a.map((bookmark, index) => {
+			{bookmarks &&
+				bookmarks.map((bookmark, index) => {
 					return <Bookmark bookmark={bookmark} enableEdit={true} key={index} />;
 				})}
 		</div>
