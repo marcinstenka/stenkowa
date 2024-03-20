28c283745c3c4ed30d5e8418f3d62da3895b1a0a
marcinstenka <mastenka@wp.pl>
2024-03-20 16:57:27 +0100

    todos select from database

--- stenkowa/app/lib/functions/data.ts
+++ stenkowa/app/lib/functions/data.ts
@@ -1,8 +1,9 @@
 import { sql } from '@vercel/postgres';
 import { unstable_noStore as noStore } from 'next/cache';
-import { BookmarkType } from '../types/types';
+import { BookmarkType, TodoType } from '../types/types';
 
 const USER_ID = 1; // for testing
+const TODOS_CONTAINER_ID = 1; // for testing
 
 export async function fetchBookmarks() {
 	noStore();
@@ -22,6 +23,17 @@ export async function fetchBookmark(id: number) {
         select * FROM bookmarks where user_id = ${USER_ID} and id = ${id}
         `;
 		return data.rows[0];
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function fetchTodos() {
+	noStore();
+	try {
+		const data = await sql<TodoType>`
+        select * FROM todos where todos_container_id = ${TODOS_CONTAINER_ID}`;
+		return data.rows;
 	} catch (error) {
 		console.log(error);
 	}
