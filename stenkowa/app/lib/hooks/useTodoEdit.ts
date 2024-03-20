469cd0ecd6d20a5203f6327e4b67ad45caa64374
marcinstenka <mastenka@wp.pl>
2024-03-20 21:28:14 +0100

    todo time left fixed & todo time display on edit

--- stenkowa/app/lib/hooks/useTodoEdit.ts
+++ stenkowa/app/lib/hooks/useTodoEdit.ts
@@ -8,16 +8,14 @@ export default function useTodoEdit(todo: TodoType) {
 	const [date_added, setDate_added] = useState(todo.date_added);
 	const [date_deadline, setDate_deadline] = useState(todo.date_deadline);
 	const [timeLeft, setTimeLeft] = useState(
-		calculateTimedifference(todo.date_added, todo.date_deadline)
+		calculateTimedifference(todo.date_deadline)
 	);
 
 	// updaing time left live
 	useEffect(() => {
-		const new_date_added = new Date(date_added);
 		const new_date_deadline = new Date(date_deadline);
-
-		setTimeLeft(calculateTimedifference(new_date_added, new_date_deadline));
-	}, [date_added, date_deadline]);
+		setTimeLeft(calculateTimedifference(new_date_deadline));
+	}, [date_deadline]);
 
 	const handleHeaderChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setDetails_header(e.target.value);
