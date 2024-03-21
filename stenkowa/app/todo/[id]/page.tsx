5b34b71cc47d9c19f7b6bdbbf711ab764ef7e249
marcinstenka <mastenka@wp.pl>
2024-03-21 19:58:27 +0100

    todo time left displaying minus 1 hour & todo deatails rerender

--- stenkowa/app/todo/[id]/page.tsx
+++ stenkowa/app/todo/[id]/page.tsx
@@ -1,5 +1,4 @@
 import TodoDetails from '@/app/lib/components/todo/TodoDetails';
-
 export default function Page({ params }: { params: { id: number } }) {
-	return <TodoDetails id={params.id} />;
+	return <TodoDetails id={params.id} key={params.id} />;
 }
