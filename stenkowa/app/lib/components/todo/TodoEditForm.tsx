c3dae326af170ab6caf531b6a5742bc941643180
marcinstenka <mastenka@wp.pl>
2024-03-24 12:34:52 +0100

    todo edit date changed to ISO

--- stenkowa/app/lib/components/todo/TodoEditForm.tsx
+++ stenkowa/app/lib/components/todo/TodoEditForm.tsx
@@ -90,9 +90,7 @@ export default function TodoEditForm(todo: TodoType) {
 							type='datetime-local'
 							name='date_added'
 							id='date_added'
-							defaultValue={date_added
-								.toLocaleString('sv', { timeZone: 'Europe/Warsaw' })
-								.replace(',', '')}
+							defaultValue={date_added.toISOString().slice(0, 16)}
 							onChange={handleDateAddedChange}
 							readOnly
 						/>
@@ -102,9 +100,7 @@ export default function TodoEditForm(todo: TodoType) {
 							type='datetime-local'
 							name='date_deadline'
 							id='date_deadline'
-							defaultValue={date_deadline
-								.toLocaleString('sv', { timeZone: 'Europe/Warsaw' })
-								.replace(',', '')}
+							defaultValue={date_deadline.toISOString().slice(0, 16)}
 							style={{ borderColor: `${color}` }}
 							color-changing='border-color'
 							onChange={handleDateDeadlineChange}
