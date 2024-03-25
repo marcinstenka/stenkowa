8435fe4db84e7c65cb98e754888b42f4efece1fb
marcinstenka <mastenka@wp.pl>
2024-03-25 11:13:23 +0100

    todo update - back to default ISO string

--- stenkowa/app/lib/functions/actions.ts
+++ stenkowa/app/lib/functions/actions.ts
@@ -154,17 +154,14 @@ export async function updateTodo(todoId: number, formData: FormData) {
 		date_deadline_string: formData.get('date_deadline')?.toString(),
 	};
 	const { name, description, color, date_deadline_string } = validatedFields;
-	let deadline: Date;
-	let uniq_deadline: string = '';
+	let deadline: string = '';
 	if (date_deadline_string) {
-		deadline = new Date(date_deadline_string);
-		deadline.setSeconds(deadline.getUTCSeconds() + 1);
-		uniq_deadline = deadline.toISOString();
+		deadline = new Date(date_deadline_string).toISOString();
 	}
 
 	try {
 		await sql`
-		UPDATE todos SET name = ${name}, description = ${description}, color = ${color}, date_deadline = ${uniq_deadline} WHERE id = ${todoId}`;
+		UPDATE todos SET name = ${name}, description = ${description}, color = ${color}, date_deadline = ${deadline} WHERE id = ${todoId}`;
 	} catch (error) {
 		console.log(error);
 	}
