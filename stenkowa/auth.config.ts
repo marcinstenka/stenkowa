a3d2d86f15efbdcf72fc049a6a6ee605d9cfe1f6
marcinstenka <mastenka@wp.pl>
2024-04-12 15:22:04 +0200

    auth.config.ts added

---
+++ stenkowa/auth.config.ts
@@ -0,0 +1,39 @@
+import type { NextAuthConfig } from 'next-auth';
+
+export const authConfig = {
+	pages: {
+		signIn: '/login',
+	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			const isOnTodos = nextUrl.pathname.startsWith('/todos');
+			const isOnStorage = nextUrl.pathname.startsWith('/storage');
+			const isOnBookmarks = nextUrl.pathname.startsWith('/bookmarks');
+
+			if (isOnTodos) {
+				if (isLoggedIn) return true;
+				return false;
+			} else if (isLoggedIn) {
+				return Response.redirect(new URL('/dashboard', nextUrl));
+			}
+
+			if (isOnStorage) {
+				if (isLoggedIn) return true;
+				return false;
+			} else if (isLoggedIn) {
+				return Response.redirect(new URL('/storage', nextUrl));
+			}
+
+			if (isOnBookmarks) {
+				if (isLoggedIn) return true;
+				return false;
+			} else if (isLoggedIn) {
+				return Response.redirect(new URL('/bookmarks', nextUrl));
+			}
+
+			return true;
+		},
+	},
+	providers: [],
+} satisfies NextAuthConfig;
