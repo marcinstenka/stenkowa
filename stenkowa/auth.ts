556826f9f2037b95e5d915b5bc3df0fb178cc8f3
marcinstenka <mastenka@wp.pl>
2024-04-15 09:33:15 +0200

    removed unnecessary code

--- stenkowa/auth.ts
+++ stenkowa/auth.ts
@@ -31,7 +31,6 @@ export const { auth, signIn, signOut } = NextAuth({
 					if (!user) return null;
 					const passwordsMatch = await bcrypt.compare(password, user.password);
 					if (passwordsMatch) return user;
-					redirect('/');
 				}
 				console.log('Invalid credentials');
 
