c1a41270d8079c11d198ec64f872655b2472af04
marcinstenka <mastenka@wp.pl>
2024-04-18 06:34:07 +0200

    useColorChanging hook types

--- stenkowa/app/lib/hooks/useColorChanging.ts
+++ stenkowa/app/lib/hooks/useColorChanging.ts
@@ -1,18 +1,20 @@
 import { ChangeEvent, useEffect, useState } from 'react';
 
-export default function useColorChanging(value: string) {
-	let colorValue: string = '#0050b8';
-	if (value) colorValue = value;
-	const [color, setColor] = useState(colorValue);
+export default function useColorChanging(value: string = '#0050b8') {
+	const [color, setColor] = useState<string>(value);
+
 	useEffect(() => {
-		const colorChangingElements = document.querySelectorAll('[color-changing]');
-		colorChangingElements.forEach((element) => {
+		const colorChangingElements: NodeListOf<Element> =
+			document.querySelectorAll('[color-changing]');
+
+		colorChangingElements.forEach((element: Element) => {
 			element.setAttribute(
 				'style',
 				`${element.getAttribute('color-changing')}: ${color}`
 			);
 		});
 	}, [color]);
+	
 	const handleColorChange = (e: ChangeEvent<HTMLInputElement>) => {
 		setColor(e.target.value);
 	};
